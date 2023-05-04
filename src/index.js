c268ac75e54647018265b0d09c7d7f980502fbd2
Tetiana Siestova <siestova.t@gmail.com>
2023-05-04 20:24:22 +0300

    modal

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import "./style.scss";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const openBtn = document.querySelector("#open");
+  const modal = document.querySelector("[data-modal]");
+
+  openBtn.addEventListener("click", () => {
+    modal.showModal();
+  });
+});
